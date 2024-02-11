600e69ca5e040e5e086f23b09be0fb2dab037495
Maxime MUNARI <88986446+MaxMnr@users.noreply.github.com>
2024-02-11 21:44:57 +0100

    Add files via upload

---
+++ scripts/fourier.js
@@ -0,0 +1,250 @@
+let points;
+let coef;
+let path;
+let time;
+
+let run;
+let mouseDown;
+let shift;
+
+function setup() {
+  let can = createCanvas(windowWidth * 0.5, windowHeight * 0.7);
+  can.parent("project-fourier-animation");
+  frameRate(60);
+  shift = 0;
+
+  buttonRun = createButton("Play").mousePressed(start);
+  buttonReset = createButton("Clear").mousePressed(reset);
+  sliderSpeed = createSlider(1, 60, 40, 1);
+  sliderNbrCircles = createSlider(1, 2, 2, 1);
+
+  speedText = createDiv("");
+
+  circleText = createDiv("");
+
+  buttonRun.parent("project-fourier-widgets");
+  buttonReset.parent("project-fourier-widgets");
+  sliderSpeed.parent("project-fourier-widgets");
+
+  speedText.parent("project-fourier-widgets");
+  sliderNbrCircles.parent("project-fourier-widgets");
+
+  circleText.parent("project-fourier-widgets");
+
+  buttonRun.addClass("button");
+  buttonReset.addClass("button");
+  sliderSpeed.addClass("slider");
+  speedText.addClass("text");
+  sliderNbrCircles.addClass("slider");
+  circleText.addClass("text");
+
+  init();
+}
+
+function draw() {
+  background(0);
+  frameRate(sliderSpeed.value());
+
+  speedText.html(
+    '<span style="color: #fffffe;">' +
+      "Speed (fps): " +
+      "</span>" +
+      '<span style="color: #7f5af0;">' +
+      str(sliderSpeed.value()) +
+      "</span>"
+  );
+
+  circleText.html(
+    '<span style="color: #fffffe;">' +
+      "Number of Circles: " +
+      "</span>" +
+      '<span style="color: #7f5af0;">' +
+      str(sliderNbrCircles.value()) +
+      "</span>"
+  );
+
+  if (!run) {
+    if (points.length == 0) {
+      push();
+      fill("white");
+      noStroke();
+      textFont("Computer Modern");
+      textSize(26);
+      textAlign(CENTER, CENTER);
+      text("Draw something here", width / 2, height / 2);
+      pop();
+    }
+    if (mouseDown) {
+      points.push(createVector(mouseX, mouseY));
+    }
+
+    for (let i = 0; i < points.length - 1; i++) {
+      let pt1 = points[i];
+      let pt2 = points[i + 1];
+      stroke("white");
+      strokeWeight(2);
+      line(pt1.x, pt1.y, pt2.x, pt2.y);
+    }
+  } else {
+    let x = 0;
+    let y = 0;
+    for (let i = 0; i < coef.length; i++) {
+      let prevx = x;
+      let prevy = y;
+
+      let frequency = coef[i].freq;
+      let radius = coef[i].amp;
+      let arg = coef[i].phase;
+      if (i <= sliderNbrCircles.value()) {
+        x += radius * cos(frequency * time + arg);
+        y += radius * sin(frequency * time + arg);
+
+        if (i != 0) {
+          push();
+          noFill();
+          stroke("white");
+          strokeWeight(0.5);
+          circle(prevx - shift, prevy, 2 * radius);
+          line(prevx - shift, prevy, x - shift, y);
+          pop();
+        }
+      }
+    }
+    path.push(createVector(x, y));
+
+    push();
+    noFill();
+    beginShape();
+    stroke(100);
+    strokeWeight(0.6);
+    line(x - shift, y, x, y);
+    for (let i = 0; i < points.length; i++) {
+      vertex(points[i].x, points[i].y);
+    }
+    endShape();
+    pop();
+
+    push();
+    noFill();
+    beginShape();
+    for (let i = 0; i < path.length; i++) {
+      vertex(path[i].x, path[i].y);
+    }
+    endShape();
+    pop();
+
+    if (path.length > coef.length * 0.9) {
+      path.shift();
+    }
+    time += (2 * PI) / points.length;
+  }
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth * 0.5, windowHeight * 0.7);
+}
+
+function mousePressed() {
+  if (mouseOnCanvas()) {
+    mouseDown = true;
+  }
+}
+
+function mouseReleased() {
+  mouseDown = false;
+}
+
+function mouseOnCanvas() {
+  return mouseX > shift && mouseX < width && (mouseY > 0) & (mouseY < height);
+}
+
+function init() {
+  points = [];
+  coef = [];
+  path = [];
+  time = 0;
+  run = false;
+  mouseDown = false;
+  sliderSpeed.value(40);
+}
+
+function start() {
+  run = true;
+  coef = dft(points);
+  sliderNbrCircles.attribute("max", points.length);
+  sliderNbrCircles.value(points.length);
+}
+
+function reset() {
+  init();
+}
+
+function dft(points) {
+  let N = points.length;
+  let coef = [];
+  for (let k = 0; k < N; k++) {
+    let re = 0;
+    let im = 0;
+    for (let n = 0; n < N; n++) {
+      let pt = points[n];
+      let omega = (TWO_PI * k * n) / N;
+      re += pt.x * cos(omega) + pt.y * sin(omega);
+      im -= pt.x * sin(omega) - pt.y * cos(omega);
+    }
+    re = re / N;
+    im = im / N;
+
+    let freq = k;
+    let amp = sqrt(re * re + im * im);
+    let phase = atan2(im, re);
+    coef.push({ re, im, freq, amp, phase });
+  }
+  //Sort the coeff by decreasing mag
+  coef = coef.sort((a, b) => b.amp - a.amp);
+  return coef;
+}
+
+function makeCircles(coefficients) {
+  let circles = [];
+  let x = 0;
+  let y = 0;
+  for (let i = 0; i < coefficients.length; i++) {
+    let coef = coefficients[i];
+    let freq = coef.freq;
+    let phase = coef.phase;
+    let rad = coef.amp;
+    let circ = new Circle(x, y, rad, freq, phase);
+
+    x += rad * cos(phase);
+    y += rad * sin(phase);
+
+    circles.push(circ);
+  }
+
+  return circles;
+}
+
+class Circle {
+  constructor(x, y, rad, frequency, phase) {
+    this.pos = createVector(x, y);
+    this.rad = rad;
+    this.frequency = frequency;
+    this.phase = phase;
+  }
+
+  update() {
+    this.pos.x = this.rad * cos(this.frequency * time + this.phase);
+    this.pos.y = this.rad * sin(this.frequency * time + this.phase);
+  }
+
+  show() {
+    push();
+    noFill();
+    //line(this.pos.x, this.pos.y,
+    //   this.pos.x + this.rad * cos(this.phase),
+    // this.pos.y + this.rad * sin(this.phase));
+    circle(this.pos.x, this.pos.y, 2 * this.rad);
+    pop();
+  }
+}
+
---
+++ scripts/ising.js
@@ -0,0 +1,257 @@
+let res;
+
+let spins = [-1, 1];
+let grid = [];
+let row, col;
+
+let sliderRes;
+let sliderTemp;
+let T;
+let run = 0;
+
+function setup() {
+  let can = createCanvas(windowHeight * 0.8, windowHeight * 0.8);
+  can.parent("project-ising-animation");
+
+  buttonStart = createButton("Start").mousePressed(startB);
+  buttonReset = createButton("Reset").mousePressed(reset);
+  sliderTemp = createSlider(0.1, 5, 2.27, 0.01);
+  sliderRes = createSlider(10, 100, 20, 1);
+
+  tempText = createDiv("");
+  resText = createDiv("");
+
+  buttonStart.parent("project-ising-widgets");
+  buttonReset.parent("project-ising-widgets");
+  sliderTemp.parent("project-ising-widgets");
+  tempText.parent("project-ising-widgets");
+  sliderRes.parent("project-ising-widgets");
+  resText.parent("project-ising-widgets");
+
+  buttonStart.addClass("button");
+  buttonReset.addClass("button");
+  sliderTemp.addClass("slider");
+  tempText.addClass("text");
+  sliderRes.addClass("slider");
+  resText.addClass("text");
+
+  frameRate(20);
+  res = int(sliderRes.value());
+  col = res;
+  row = res;
+  grid = [];
+  for (let i = 0; i < row + 1; i++) {
+    let new_row = [];
+    for (let j = 0; j < col + 1; j++) {
+      new_row.push(spins[int(random(2))]);
+    }
+    grid.push(new_row);
+  }
+}
+function startB() {
+  run = (run + 1) % 2;
+  if (run % 2 == 1) {
+    buttonStart.html("Stop");
+  } else {
+    buttonStart.html("Start");
+  }
+}
+function resetup() {
+  grid = [];
+  res = sliderRes.value();
+  col = res;
+  row = res;
+
+  for (let i = 0; i < row + 1; i++) {
+    let new_row = [];
+    for (let j = 0; j < col + 1; j++) {
+      new_row.push(spins[int(random(2))]);
+    }
+    grid.push(new_row);
+  }
+}
+function draw() {
+  background(0);
+
+  T = sliderTemp.value();
+  tempText.html(
+    '<span style="color: #fffffe;">' +
+      "Temperature: " +
+      "</span>" +
+      '<span style="color: #7f5af0;">' +
+      str(T) +
+      "</span>"
+  );
+  resText.html(
+    '<span style="color: #fffffe;">' +
+      "Grid Size: " +
+      "</span>" +
+      '<span style="color: #7f5af0;">' +
+      str(res) +
+      "</span>"
+  );
+
+  if (int(sliderRes.value()) != int(res)) {
+    res = sliderRes.value();
+    resetup();
+  }
+
+  if (run == 1) {
+    //Simulate the evolution of the system using Glauber Dynamics
+    for (let i = 0; i < grid.length * grid[0].length; i++) {
+      let x = int(random(grid[0].length));
+      let y = int(random(grid.length));
+      let S =
+        grid[y][mod(x + 1, grid[0].length)] +
+        grid[y][mod(x - 1, grid[0].length)] +
+        grid[mod(y + 1, grid.length)][x] +
+        grid[mod(y - 1, grid.length)][x];
+      let dE = 2 * grid[y][x] * S;
+      let proba = 1 / (1 + exp(dE / T));
+      if (random(1) < proba) {
+        grid[y][x] *= -1;
+      }
+    }
+  }
+
+  //Display the grid using Marching Squares
+  let w = width / col;
+  let h = height / row;
+  for (let i = 0; i < row + 1; i++) {
+    for (let j = 0; j < col + 1; j++) {
+      push();
+      strokeWeight(1);
+      if (grid[i][j] == -1) {
+        stroke("#7f5af0");
+        noFill();
+        circle(j * w, i * h, w / 2);
+        point(j * w, i * h);
+      }
+      if (grid[i][j] == 1) {
+        stroke("#2cb67d");
+        cross(j * w, i * h, w / 2);
+      }
+      //point(j * res + res/2, i * res + res/2)
+      pop();
+
+      let TL = grid[i][j];
+      let TR = grid[i][mod(j + 1, grid.length)];
+      let DR = grid[mod(i + 1, grid[0].length)][mod(j + 1, grid.length)];
+      let DL = grid[mod(i + 1, grid[0].length)][j];
+
+      let squareType = getSquareType(TL, TR, DR, DL);
+
+      let a = createVector(j * w + w / 2, i * h);
+      let b = createVector(j * w + w, i * h + h / 2);
+      let c = createVector(j * w + w / 2, i * h + h);
+      let d = createVector(j * w, i * h + h / 2);
+
+      stroke("white");
+      switch (squareType) {
+        case 0:
+          break;
+        case 1:
+          drawLine(c, d);
+          break;
+        case 2:
+          drawLine(b, c);
+          break;
+        case 3:
+          drawLine(b, d);
+          break;
+        case 4:
+          drawLine(a, b);
+          break;
+        case 5:
+          drawLine(a, d);
+          drawLine(b, c);
+          break;
+        case 6:
+          drawLine(a, c);
+          break;
+        case 7:
+          drawLine(a, d);
+          break;
+        case 8:
+          drawLine(a, d);
+          break;
+        case 9:
+          drawLine(a, c);
+          break;
+        case 10:
+          drawLine(a, b);
+          drawLine(c, d);
+          break;
+        case 11:
+          drawLine(a, b);
+          break;
+        case 12:
+          drawLine(b, d);
+          break;
+        case 13:
+          drawLine(b, c);
+          break;
+        case 14:
+          drawLine(c, d);
+          break;
+        case 15:
+          break;
+      }
+    }
+  }
+}
+
+function windowResized() {
+  resizeCanvas(windowHeight * 0.8, windowHeight * 0.8);
+  resetup();
+}
+
+function reset() {
+  grid = [];
+  for (let i = 0; i < row + 1; i++) {
+    let new_row = [];
+    for (let j = 0; j < col + 1; j++) {
+      new_row.push(spins[int(random(2))]);
+    }
+    grid.push(new_row);
+  }
+}
+
+function getSquareType(c1, c2, c3, c4) {
+  let b1 = c1;
+  let b2 = c2;
+  let b3 = c3;
+  let b4 = c4;
+
+  if (b1 == -1) {
+    b1 = 0;
+  }
+  if (b2 == -1) {
+    b2 = 0;
+  }
+  if (b3 == -1) {
+    b3 = 0;
+  }
+  if (b4 == -1) {
+    b4 = 0;
+  }
+  return int(b1 * 8 + b2 * 4 + b3 * 2 + b4 * 1);
+}
+
+function drawLine(vec1, vec2) {
+  line(vec1.x, vec1.y, vec2.x, vec2.y);
+}
+
+function mod(x, m) {
+  let res = x;
+  while (res < 0) {
+    res += m;
+  }
+  return res % m;
+}
+
+function cross(x, y, s) {
+  line(x - s / 2, y - s / 2, x + s / 2, y + s / 2);
+  line(x - s / 2, y + s / 2, x + s / 2, y - s / 2);
+}
+
